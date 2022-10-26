1acedde7f5bccd5e9fdf1f1b0f52932de4cc963c
Sameer Asad <51676937+sameerasad@users.noreply.github.com>
2022-10-25 17:54:14 -0700

    AUTH AND FORGOT SCREENS

---
+++ src/screens/Auth/ForgotPassword.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ForgotPassword = ({step=false}) => {
+  return (
+    <View>
+      <Text
+      style={{color:"black"}}
+      >ForgotPassword</Text>
+    </View>
+  )
+}
+
+export default ForgotPassword
